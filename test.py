4b110795cd3dad2c7aac9c031c9a4a270b382778
Guillermo A. Perez <gaperez64@gmail.com>
2024-03-10 16:14:42 +0100

    started plotting stuff

--- test.py
+++ test.py
@@ -3,6 +3,9 @@
 from cmsimpy import BECovid19, SIR
 
 
+noSims = 1000
+
+
 def simulateBECovid19():
     model = BECovid19.BECovid19(h=1.0 / 24.0,
                                 qcsym=0.051 * 1.52,
@@ -22,16 +25,22 @@ def simulateBECovid19():
 
 
 def simulateSIR():
-    model = SIR.SIR(h=1.0/24.0,
-                    beta=1.0/2.0,
-                    gamma=1.0/6.0)
-    state = {SIR.Compartment.SUSCEPTIBLE: 3,
-             SIR.Compartment.INFECTIOUS: 1,
-             SIR.Compartment.RECOVERED: 0}
-    print("Simulating")
-    for _ in range(100):
-        state = model.step(state)
-        print(state)
+    model = SIR.SIR(h=1.0 / 24.0,
+                    beta=1.0 / 2.0,
+                    gamma=0.729)
+    trajectories = []
+    for i in range(noSims):
+        if i % 10 == 0:
+            print(f"Simulation no. {i + 1}")
+        state = {SIR.Compartment.SUSCEPTIBLE: 10,
+                 SIR.Compartment.INFECTIOUS: 100,
+                 SIR.Compartment.RECOVERED: 0}
+        trajectories.append([state])
+        while state[SIR.Compartment.INFECTIOUS] > 0:
+            state = model.step(state)
+            trajectories[-1].append(state)
+
+    # Now we can plot the averages per timestep
 
 
 if __name__ == "__main__":
